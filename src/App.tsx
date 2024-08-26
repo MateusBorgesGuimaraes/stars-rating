6240597bcd6f053aa68ef71b224a7d446e8297c9
MBguimaraes <104425878+MateusBorgesGuimaraes@users.noreply.github.com>
2024-08-26 13:23:14 -0300

    Feito o bruto,em algum momento refatorar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import StarsRating from './StarRating';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <StarsRating percent="72" size="24" color="#EAB308" type="pointed" />
+    <div className="container">
+      <div className="img-prod"></div>
+      <div className="infos">
+        <div>
+          <StarsRating percent="72" size="24" color="#EAB308" type="pointed" />
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+        <button>comprar</button>
+      </div>
     </div>
   );
 }
